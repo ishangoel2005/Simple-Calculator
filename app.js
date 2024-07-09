d5ffe28adbda384e9514166510724da541700e8d
ishangoel2005 <ishangoel2005@gmail.com>
2024-07-09 11:01:47 -0700

    uploaded files

---
+++ app.js
@@ -0,0 +1,16 @@
+function clearDisplay() {
+    document.getElementById('display').innerText = '';
+}
+
+function appendToDisplay(value) {
+    document.getElementById('display').innerText += value;
+}
+
+function calculateResult() {
+    const display = document.getElementById('display');
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = 'Error';
+    }
+}
